cad218012dc8c83f95315e406e0462bef952b9c3
Arthur David <32845935+arthurdlima@users.noreply.github.com>
2020-06-20 00:59:03 -0300

    working on classes

---
+++ bkp_main.kt
@@ -0,0 +1,18 @@
+
+fun main() {
+  var x = 10
+  var y = 20
+
+  fun sum(): Int {
+    var x = 30
+    var y = 40
+  }
+
+  fun sub(): Int {
+    var x = 50
+    var y = 60
+  }
+  sum()
+  sub()
+  var z = 70
+}
--- main.kt
+++ main.kt
@@ -1,11 +1,7 @@
-
+// PAREI AULA 4 2H 40 MIN MAIS OU MENOS
 fun main() {
-  var x = 1
-  var y = 2
-  fun sum(): Int {
-    var x = 60
-    var y = 90
+  fun sum(a): Int {
+    var x = 40
   }
-
-  sum()
+sum(1);
 }
